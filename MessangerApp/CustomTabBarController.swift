4f3f0a456cc8a14aa9c1ae9fadecd8c6c24b170b
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-06-21 21:51:01 -0700

    10:03 add multiple tab view with function createDummyNavController

--- MessangerApp/CustomTabBarController.swift
+++ MessangerApp/CustomTabBarController.swift
@@ -20,13 +20,29 @@ class CustomTabBarController: UITabBarController {
         recentMessagesNavController.tabBarItem.title = "Recent"
         recentMessagesNavController.tabBarItem.image = UIImage(named: "recent")
         
+//        let viewController = UIViewController()
+//        let navController = UINavigationController(rootViewController: viewController)
+//        navController.tabBarItem.title = "Calls"
+//        navController.tabBarItem.image = UIImage(named: "calls")
+        
+        viewControllers = [recentMessagesNavController, createDummyNavControllerWithTitle("Calls", imageName: "calls"), createDummyNavControllerWithTitle("Groups", imageName: "groups"), createDummyNavControllerWithTitle("People", imageName: "people"), createDummyNavControllerWithTitle("Settings", imageName: "settings")]
+    }
+    
+    private func createDummyNavControllerWithTitle(title: String, imageName: String) -> UINavigationController {
         let viewController = UIViewController()
         let navController = UINavigationController(rootViewController: viewController)
-        navController.tabBarItem.title = "Calls"
-        navController.tabBarItem.image = UIImage(named: "calls")
-        
-        
-        
-        viewControllers = [recentMessagesNavController, navController]
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = UIImage(named: imageName)
+        return navController
     }
 }
+
+
+
+
+
+
+
+
+
+
